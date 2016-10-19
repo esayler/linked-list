aa593f95f538c48c7d4f764d779d9cefe8e41798
Eric Sayler <easayler@gmail.com>
2016-10-19 12:40:29 -0600

    add read-button and delete-button functionality

--- scripts/linked-list.js
+++ scripts/linked-list.js
@@ -7,23 +7,34 @@ $('#create-button').on('click', function() {
     var url = $('#url-form').val();
     count++;
 
-    $('.list').append('<div id=>\
-                      <h1 class="bookmark-title"> ' + title + '</h1>\
-                      <p class=".bookmark-url">' + url + '</p>\
-                      </div>')
+    $('.list').append('<article class="box" id="bookmark-' + count + '">\
+                         <h1 class="bookmark-title"> ' + title + '</h1>\
+                           <a class=".bookmark-url" href="' + url + '">\
+                             <p>' + url + '</p>\
+                           </a>\
+                         <div class="bookmark-buttons">\
+                            <button type="button" name="Read" class="read-button">\
+                              <p>Read</p>\
+                            </button>\
+                            <button type="button" name="Delete" class="delete-button">\
+                               <p>Delete</p>\
+                            </button>\
+                          </div>\
+                       </article>');
 
 });
 
 
 // each bookmark should have a "Mark as Read" button
-$('.read-button').on('click', function() {
-
+$(document).on('click', '.read-button', function() {
+  $(this).parents('article').addClass('read');
 });
 
 
 // each bookmark should have a "Remove" button
-$('.delete-button').on('click', function() {
-
+//$('.delete-button').on('click', function() {
+$(document).on('click', '.delete-button', function() {
+  $(this).parents('article').remove();
 });
 
 $('bookmark-title')
